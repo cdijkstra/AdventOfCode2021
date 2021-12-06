3d8e4ac5405f6963cd758449874cbfd6912f5e0b
cdijkstra <cdijkstra@xpirit.com>
2021-12-06 22:25:44 +0100

    Solution for Day1-2

--- Day1/day1.py
+++ Day1/day1.py
@@ -2,11 +2,20 @@ file = open('data.txt')
 lines = file.readlines()
 
 totalCount = 0
-previousLine = 'Unknown'
+lineMinusTwo = 'Unknown'
+LineMinusOne = 'Unknown'
+previousSum = 'Unknown'
+
 for line in lines:
-    intLine = int(line)
-    if previousLine != 'Unknown' and intLine - previousLine > 0:
-        totalCount += 1
-    previousLine = intLine
+    newLine = int(line)
+    if lineMinusTwo != 'Unknown' and LineMinusOne != 'Unknown':
+        sum = newLine + LineMinusOne + lineMinusTwo
+        if previousSum != 'Unknown':
+            if sum - previousSum > 0:
+                totalCount += 1
+        previousSum = sum
+
+    lineMinusTwo = LineMinusOne
+    LineMinusOne = newLine
     
 print(totalCount)
