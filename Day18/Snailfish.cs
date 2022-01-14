dc7c8a3b6bf5228716cdcf50c8a1b604bc719425
cdijkstra <cdijkstra@xpirit.com>
2022-01-14 21:21:18 +0100

    Solution almost working!

--- Day18/Snailfish.cs
+++ Day18/Snailfish.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
 using System.Linq;
+using System.Text;
 
 namespace Day18.Input
 {
     public class Snailfish
     {
         private List<string> _snailFish;
-        private char[] _currentSnailFish;
+        private string _currentSnailFish;
         public Snailfish(string fileName)
         {
             _snailFish = new List<string>(File.ReadAllLines($"../../../Input/{fileName}"));
@@ -17,14 +18,19 @@ namespace Day18.Input
 
         public void FindSolution()
         {
-            _currentSnailFish = _snailFish.First().ToCharArray();
+            _currentSnailFish = _snailFish.First();
             while (!ExplodesDone())
             {
-                Console.Write("Try again exploding");
+                Console.WriteLine("Try again exploding");
             }
             while (!ReplacePairsDone())
             {
-                Console.Write("Try again replacing");
+                Console.WriteLine("Try again replacing");
+            }
+
+            foreach (var test in _currentSnailFish)
+            {
+                Console.Write(test);
             }
         }
 
@@ -35,36 +41,43 @@ namespace Day18.Input
             foreach (var idx in Enumerable.Range(1, _currentSnailFish.Length - 1))
             {
                 charsSoFar = charsSoFar.Append(_currentSnailFish[idx]).ToArray();
-                if (charsSoFar.Count(x => x == '[') - charsSoFar.Count(x => x == ']') > 3)
+                if (charsSoFar.Count(x => x == '[') - charsSoFar.Count(x => x == ']') > 4)
                 {
                     // Replace left side if applicable
                     int leftInt;
-                    var successParseLeft = int.TryParse(_currentSnailFish[idx + 1].ToString(), out leftInt);
+                    int leftIntIndex = idx + 1;
+                    var successParseLeft = int.TryParse(_currentSnailFish[leftIntIndex].ToString(), out leftInt);
                     if (!successParseLeft)
                         continue;
-                    var down = idx - 1;
+                    var down = leftIntIndex - 1;
                     bool replaceLeft = false;
 
                     bool extraAddedLeft = false;
                     
-                    while (down != 0 && !replaceLeft)
+                    while (down != 0 && replaceLeft == false)
                     {
                         if (char.IsDigit(_currentSnailFish[down]))
                         {
+                            var strBuilder = new StringBuilder(_currentSnailFish);
+                            
                             var replaceInt = int.Parse(_currentSnailFish[down].ToString());
                             if (replaceInt + leftInt <= 9)
                             {
-                                _currentSnailFish[down] = (char) ('0' + replaceInt + leftInt);
+                                strBuilder.Remove(down, 1);
+                                strBuilder.Insert(down, (char) ('0' + replaceInt + leftInt));
                             }
                             else
                             {
                                 var twoChars = (replaceInt + leftInt).ToString().ToCharArray();
-                                _currentSnailFish[down] = twoChars[0];
-                                _currentSnailFish[down + 1] = twoChars[1];
+                                
+                                strBuilder.Remove(down, 1);
+                                strBuilder.Insert(down, twoChars[0]);
+                                strBuilder.Insert(down + 1, twoChars[1]);
 
                                 extraAddedLeft = true;
                             }
                             
+                            _currentSnailFish = strBuilder.ToString();
                             replaceLeft = true;
                         }
 
@@ -73,37 +86,50 @@ namespace Day18.Input
                     
                     // Replace right side if applicable
                     int rightInt;
-                    var successParseRight = int.TryParse(_currentSnailFish[idx + 1].ToString(), out rightInt);
+                    int rightIntIndex = idx + 3 + Convert.ToInt32(extraAddedLeft);
+                    var successParseRight = int.TryParse(_currentSnailFish[rightIntIndex].ToString(), out rightInt);
                     if (!successParseRight)
                         continue;
-                    var up = idx + 3 + Convert.ToInt32(extraAddedLeft);
+                    var up = rightIntIndex + 1;
                     bool replaceRight = false;
                     bool addedExtraRight = false;
-                    while (up != _currentSnailFish.Length && !replaceRight)
+                    while (up != _currentSnailFish.Length && replaceRight == false)
                     {
                         if (char.IsDigit(_currentSnailFish[up]))
                         {
+                            var strBuilder = new StringBuilder(_currentSnailFish);
                             var replaceInt = int.Parse(_currentSnailFish[up].ToString());
+                            
                             if (replaceInt + rightInt < 9)
                             {
-                                _currentSnailFish[up] = (char) ('0' + replaceInt + rightInt);
+                                strBuilder.Remove(up, 1);
+                                strBuilder.Insert(up, (char) ('0' + replaceInt + rightInt));
                             }
                             else
                             {
                                 var twoChars = (replaceInt + rightInt).ToString().ToCharArray();
-                                _currentSnailFish[up] = twoChars[0];
-                                _currentSnailFish[up + 1] = twoChars[1];
-                                addedExtraRight = true;
+                                
+                                strBuilder.Remove(up, 1);
+                                strBuilder.Insert(up, twoChars[0]);
+                                strBuilder.Insert(up + 1, twoChars[1]);
                             }
 
+                            _currentSnailFish = strBuilder.ToString();
                             replaceRight = true;
                         }
 
                         up++;
                     }
                     
-                    _currentSnailFish = new string(_currentSnailFish).Remove(idx + 4 + Convert.ToInt32(extraAddedLeft) + Convert.ToInt32(addedExtraRight), 1).ToCharArray();
-                    _currentSnailFish = new string(_currentSnailFish).Remove(idx + Convert.ToInt32(extraAddedLeft), 1).ToCharArray();
+                    var newStrBuilder = new StringBuilder(_currentSnailFish);
+                    newStrBuilder.Remove(rightIntIndex + 2, 1); // ,
+                    newStrBuilder.Remove(rightIntIndex + 1, 1); // ]
+                    newStrBuilder.Remove(rightIntIndex, 1); // [0-9]
+                    
+                    newStrBuilder.Remove(rightIntIndex - 2, 1); // Replace number
+                    newStrBuilder.Insert(rightIntIndex - 2, "0");
+                    newStrBuilder.Remove(rightIntIndex - 3, 1);
+                    _currentSnailFish = newStrBuilder.ToString();
                     // Start function again
                     return false;
                 }
@@ -115,20 +141,26 @@ namespace Day18.Input
 
         private bool ReplacePairsDone()
         {
-            var firstSnailFish = _snailFish.First().ToCharArray();
-            foreach (var idx in Enumerable.Range(0, firstSnailFish.Length))
+            foreach (var idx in Enumerable.Range(0, _currentSnailFish.Length))
             {
-                if (!char.IsDigit(firstSnailFish[idx]) || !char.IsDigit(firstSnailFish[idx + 1])) continue;
+                if (!char.IsDigit(_currentSnailFish[idx]) || !char.IsDigit(_currentSnailFish[idx + 1])) continue;
                 
-                var intToBeReplaced = int.Parse(firstSnailFish[idx].ToString() + firstSnailFish[idx + 1]);
+                var intToBeReplaced = int.Parse(_currentSnailFish[idx].ToString() + _currentSnailFish[idx + 1]);
                 var down = Math.Floor((double) intToBeReplaced / 2).ToString(CultureInfo.InvariantCulture).ToCharArray().First();
                 var up = Math.Ceiling((double) intToBeReplaced / 2).ToString(CultureInfo.InvariantCulture).ToCharArray().First();
-                firstSnailFish[idx] = '[';
-                firstSnailFish[idx + 1] = down;
-                firstSnailFish[idx + 2] = ',';
-                firstSnailFish[idx + 3] = up;
-                firstSnailFish[idx + 4] = ']';
-
+                
+                var strBuilder = new StringBuilder(_currentSnailFish);
+                strBuilder.Remove(idx + 4, 1);
+                strBuilder.Remove(idx + 3, 1);
+                strBuilder.Remove(idx + 2, 1);
+                strBuilder.Remove(idx + 1, 1);
+                strBuilder.Remove(idx, 1);
+                strBuilder.Insert(idx, "[");
+                strBuilder.Insert(idx + 1, down);
+                strBuilder.Insert(idx + 2, ",");
+                strBuilder.Insert(idx + 3, up);
+                strBuilder.Insert(idx + 4, "]");
+                _currentSnailFish = strBuilder.ToString();
                 return false;
             }
 
